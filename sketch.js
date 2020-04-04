8e952058f7dacd4fd3864cb64357491f81df0312
Rodrigo Gamboa <rodrigogamboatorres@gmail.com>
2020-04-03 22:43:42 -0600

    update index and sketch

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
+var canvas;
+var mic;
+
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style('z-index', '-1');
+    mic = new p5.AudioIn();
+    mic.start();
 }
 
 function draw() {
-    ellipse(mouseX, mouseY, 50, 50);
+    //fill(0);
+    //ellipse(mouseX, mouseY, 50, 50);
+    var level = mic.getLevel();
+    ellipse(width/2, height/2, level*100);
 }
