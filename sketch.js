e67c327468e59a49ce0e2f601695d24ad6641d5e
Rodrigo Gamboa <rodrigogamboatorres@gmail.com>
2020-04-03 22:54:30 -0600

    windowResized

--- sketch.js
+++ sketch.js
@@ -1,17 +1,16 @@
 var canvas;
-var mic;
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style('z-index', '-1');
-    mic = new p5.AudioIn();
-    mic.start();
 }
 
 function draw() {
     //fill(0);
-    //ellipse(mouseX, mouseY, 50, 50);
-    var level = mic.getLevel();
-    ellipse(width/2, height/2, level*width);
+    ellipse(mouseX, mouseY, 50, 50);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
