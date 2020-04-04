b9b275ac5c4459633f8ae7238719468d34ea166e
Rodrigo Gamboa <rodrigogamboatorres@gmail.com>
2020-04-04 09:34:57 -0600

    test png image

--- sketch.js
+++ sketch.js
@@ -1,14 +1,24 @@
 var canvas;
+let img1_0, img1_1;
+
+function preload() {
+  img1_0 = loadImage('images/TecnoTarot_1.0.png');
+  img1_1 = loadImage('images/TecnoTarot_1.1.png');
+}
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style('z-index', '-1');
+    imageMode(CENTER);
+    canvas.background(255, 230, 0);
 }
 
 function draw() {
     //fill(0);
-    canvas.background(255, 230, 0);
+
+    img1_0.resize(width/2, 0);
+    image(img1_0, width/2, height/2);
     ellipse(mouseX, mouseY, 50, 50);
 }
 
