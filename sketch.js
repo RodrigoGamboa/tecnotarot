8def169dc2ccd672ad975f9387ea2ad9fe8d2b1a
Rodrigo Gamboa <rodrigogamboatorres@gmail.com>
2020-04-10 13:38:26 -0500

    updated v1.0 with home and info button

--- sketch.js
+++ sketch.js
@@ -4,11 +4,15 @@ let imgsB = [];
 let numCard = 0;
 let appearVal = 255;
 let introCard = true;
+let homeCard = false;
 let flipCard = false;
+let questionCard = false;
 let inferiorMargin = 0;
 
 function preload() {
-  crystal = loadImage('images/CrystalBall.png');
+  crystal = loadImage('images/CrystalBallIconBW.png');
+  question = loadImage('images/QuestionIconBW.png');
+  home = loadImage('images/HomeBW.png');
   imgsF[0] = loadImage('images/TecnoTarot_1.0.png');
   imgsF[1] = loadImage('images/TecnoTarot_2.0.png');
   imgsF[2] = loadImage('images/TecnoTarot_3.0.png');
@@ -50,7 +54,10 @@ function setup() {
 function draw() {
     if(introCard == true) {
       imgsF[numCard].resize(0, 3*windowHeight/4);
-      image(imgsF[numCard], windowWidth/2, windowHeight/2);
+      //image(imgsF[numCard], windowWidth/2, windowHeight/2);
+
+      imgsF[0].resize(0, 3*windowHeight/4);
+      image(imgsF[0], windowWidth/2, windowHeight/2);
       //Velocidad de aparición en móvil es muy lenta. En pc está bien. Arreglar este problema antes de volver a colocar fill y rect.
       //fill(255, 230, 0, appearVal);
       //rect(windowWidth/2, windowHeight/2, imgs[0].width, imgs[0].height);
@@ -59,19 +66,34 @@ function draw() {
         introCard == false;
     }
 
-    inferiorMargin = (windowHeight - imgsF[numCard].height) / 2;
+    inferiorMargin = (windowHeight - imgsF[numCard].height)/2;
 
-    crystal.resize(0, inferiorMargin*0.8);
+    home.resize(0, inferiorMargin*0.7);
+    image(home, windowWidth/2 - imgsF[numCard].width/3, windowHeight - inferiorMargin/2);
+    crystal.resize(0, inferiorMargin*0.7);
     image(crystal, windowWidth/2, windowHeight - inferiorMargin/2);
+    question.resize(0, inferiorMargin*0.7);
+    image(question, windowWidth/2 + imgsF[numCard].width/3, windowHeight - inferiorMargin/2);
 
     if(numCard > 0) {
       imgsF[numCard].resize(0, 3*windowHeight/4);
       image(imgsF[numCard], windowWidth/2, windowHeight/2);
     }
 
+    if(homeCard == true) {
+      imgsF[0].resize(0, 3*windowHeight/4);
+      image(imgsF[0], windowWidth/2, windowHeight/2);
+    }
+
     if(flipCard == true) {
       imgsB[numCard].resize(0, 3*windowHeight/4);
       image(imgsB[numCard], windowWidth/2, windowHeight/2);
+    }
+
+    if(questionCard == true) {
+      //Cambiar el cero con la nueva carta de info
+      imgsB[0].resize(0, 3*windowHeight/4);
+      image(imgsB[0], windowWidth/2, windowHeight/2);
     }
 }
 
@@ -81,15 +103,40 @@ function windowResized() {
 }
 
 function mouseClicked() {
+  //Click en carta
   if(mouseX > (windowWidth/2 - imgsF[numCard].width/2) && mouseX < (windowWidth/2 + imgsF[numCard].width/2)) {
     if(mouseY > (windowHeight/2 - imgsF[numCard].height/2) && mouseY < (windowHeight/2 + imgsF[numCard].height/2)) {
       flipCard = true;
+      homeCard = false;
+      questionCard = false;
     }
   }
 
+  //Click en home
+  if(mouseX > (windowWidth/2 - imgsF[numCard].width/3 - home.width/2) && mouseX < (windowWidth/2 -imgsF[numCard].width/3 + home.width/2)) {
+    if(mouseY > (windowHeight - inferiorMargin/2 - home.height/2) && mouseY < (windowHeight - inferiorMargin/2 + home.height)) {
+      homeCard = true;
+      introCard = true;
+      flipCard = false;
+      questionCard = false;
+    }
+  }
+
+  //Click en random bola de cristal
   if(mouseX > (windowWidth/2 - crystal.width/2) && mouseX < (windowWidth/2 + crystal.width/2)) {
     if(mouseY > (windowHeight - inferiorMargin/2 - crystal.height/2) && mouseY < (windowHeight - inferiorMargin/2 + crystal.height)) {
       numCard = int(random(1, 13));
+      homeCard = false;
+      flipCard = false;
+      questionCard = false;
+    }
+  }
+
+  //Click en signo de pregunta
+  if(mouseX > (windowWidth/2 + imgsF[numCard].width/3 - question.width/2) && mouseX < (windowWidth/2 + imgsF[numCard].width/3 + question.width/2)) {
+    if(mouseY > (windowHeight - inferiorMargin/2 - question.height/2) && mouseY < (windowHeight - inferiorMargin/2 + question.height)) {
+      //homeCard = false;
+      questionCard = true;
       flipCard = false;
     }
   }
