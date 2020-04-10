23dc06c34ea4c0b91d1007a099077a93df9436fe
Rodrigo Gamboa <rodrigogamboatorres@gmail.com>
2020-04-09 19:58:42 -0500

    testing different dpis

--- sketch.js
+++ sketch.js
@@ -1,25 +1,60 @@
 var canvas;
-let img1_0, img1_1;
+let imgs = [];
+let button;
+let val = 0;
+
 
 function preload() {
-  img1_0 = loadImage('images/tecno2.png');
-  img1_1 = loadImage('images/tecno2.png');
+  imgs[0] = loadImage('images/TecnoTarot_1.0.png');
+  imgs[1] = loadImage('images/TecnoTarot_2.0.png');
+  imgs[2] = loadImage('images/TecnoTarot_3.0.png');
 }
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style('z-index', '-1');
+
+    button = createButton('click me');
+    //button.position(random(width), random(height));
+    button.mousePressed(randomCard);
+
     imageMode(CENTER);
     canvas.background(255, 230, 0);
 }
 
 function draw() {
-    img1_0.resize(0, height/2);
-    image(img1_0, width/2, height/2);
-    //ellipse(random(width), random(height), 50, 50);
+    //print(val);
+    imgs[val].resize(0, height/2);
+    image(imgs[val], width/2, height/2);
+    ellipse(random(width), random(height), val, val);
+}
+
+function randomCard() {
+  val = int(random(0,3));
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+
+/*
+function setup() {
+  createCanvas(100, 100);
+  background(0);
+  button = createButton('click me');
+  button.position(19, 19);
+  button.mousePressed(changeBG);
+}
+
+function draw() {
+  ellipse(width/2, height/2, val, val);
+}
+
+function changeBG() {
+  val = random(255);
+  background(val);
+  return 0;
+}
+*/
